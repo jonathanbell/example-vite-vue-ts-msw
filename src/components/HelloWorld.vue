68ce89c30bba3c3d316aff8e6fdd3eb41f95325f
Jonathan Bell <jonathanbell.ca@gmail.com>
2023-03-01 01:23:20 +0000

    Setup project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,31 @@
-<script setup lang="ts">
-import { ref } from "vue";
+<script lang="ts">
+export default {
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      apiResponse: "",
+    };
+  },
+  methods: {
+    async getDataFromApi() {
+      const rawResponse = await fetch("/api");
+      const json = await rawResponse.json();
 
-defineProps<{ msg: string }>();
-
-const count = ref(0);
+      console.log(json);
+    },
+  },
+};
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <h1>{{ msg }}</h1>
+    <button type="button" @click="getDataFromApi">Test the API + MSW</button>
+    <pre v-text="apiResponse" />
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
